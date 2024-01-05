31a99ba8f24f19c0b70fc32919298cd37e622545
grnisha <grnisha@users.noreply.github.com>
2024-01-05 12:18:04 +0000

    updated policy file name

--- terraform/apis/conferenceapi/main.tf
+++ terraform/apis/conferenceapi/main.tf
@@ -39,6 +39,6 @@ resource "azurerm_api_management_api_operation_policy" "conferenceapi" {
   api_name            = azurerm_api_management_api.conferenceapi.name
   operation_id        = "GetSessions"
   display_name        = "GetSessions Policy"
-  xml_content         = file("./policies/operation-policy.xml")
+  xml_content         = file("./policies/op-sessions-policy.xml")
   depends_on = [azurerm_api_management_api_operation.conferenceapi]
 }
