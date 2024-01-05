7c80706f48400c3fee6e6083bebcbafbaa94f647
grnisha <grnisha@users.noreply.github.com>
2024-01-05 12:15:55 +0000

    test conf api

--- terraform/apis/conferenceapi/main.tf
+++ terraform/apis/conferenceapi/main.tf
@@ -25,20 +25,10 @@ resource "azurerm_api_management_api_policy" "conferenceapi" {
   api_management_name = var.apim_name
   resource_group_name = var.rg_name
   api_name            = azurerm_api_management_api.conferenceapi.name
-  display_name        = "Conference API Policy"
   xml_content         = file("./policies/api-policy.xml")
   depends_on = [azurerm_api_management_api.conferenceapi]
 }
 
-#-----------------------------------
-# Get deployed operation GetSessions
-#-----------------------------------
-data "azurerm_api_management_api_operation" "conferenceapi" {
-  api_management_name = var.apim_name
-  resource_group_name = var.rg_name
-  api_name            = azurerm_api_management_api.conferenceapi.name
-  operation_id        = "GetSessions"
-}
 
 #-----------------------------------
 # Deploy operation policy
@@ -47,7 +37,7 @@ resource "azurerm_api_management_api_operation_policy" "conferenceapi" {
   api_management_name = var.apim_name
   resource_group_name = var.rg_name
   api_name            = azurerm_api_management_api.conferenceapi.name
-  operation_id        = data.azurerm_api_management_api_operation.conferenceapi.id
+  operation_id        = "GetSessions"
   display_name        = "GetSessions Policy"
   xml_content         = file("./policies/operation-policy.xml")
   depends_on = [azurerm_api_management_api_operation.conferenceapi]
